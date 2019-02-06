e84459004c3655e7ba1f1d03607db11444b341c1
Cameron Welter <camjwelt@gmail.com>
2019-02-06 22:39:10 +0000

    add plugin-react-hooks to enforce rules of hooks

--- eslint-config-react/index.js
+++ eslint-config-react/index.js
@@ -12,6 +12,7 @@ module.exports = {
     '@fuelrats/eslint-config',
     './rules/plugin-react',
     './rules/plugin-react-jsx',
+    './rules/plugin-react-hooks',
     './rules/plugin-jsx-a11y',
   ].map(require.resolve),
   parserOptions: {
---
+++ eslint-config-react/rules/plugin-react-hooks.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    'react-hooks',
+  ],
+  rules: {
+    // Enforce rules of hooks
+    'react-hooks/rules-of-hooks': 'error',
+  },
+}
