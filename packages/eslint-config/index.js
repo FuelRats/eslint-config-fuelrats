2f88fabc4eb6a05bba526b328aef47f9701f952d
Cameron Welter <camjwelt@gmail.com>
2020-02-11 20:48:58 +0000

    inherit purejs to reduce duplication.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -4,24 +4,9 @@
 \***************************************************************************/
 
 module.exports = {
-  env: {
-    es6: true,
-  },
   extends: [
-    './rules/bestpractices',
-    './rules/errors',
-    './rules/es6',
-    './rules/node',
-    './rules/style',
-    './rules/variables',
+    './purejs',
     './rules/plugin-babel',
-    './rules/plugin-import',
-    './rules/plugin-jsdoc',
   ].map(require.resolve),
   parser: 'babel-eslint',
-  parserOptions: {
-    codeFrame: true,
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
 }
