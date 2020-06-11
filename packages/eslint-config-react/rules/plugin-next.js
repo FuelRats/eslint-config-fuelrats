c604766c14d20eb626a4d81d6020f80f47c0cf29
Cameron Welter <camjwelt@gmail.com>
2020-06-11 07:59:39 +0000

    Add WIP optional ruleset for nextjs plugin (which isn't released yet)

---
+++ packages/eslint-config-react/rules/plugin-next.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    'next',
+  ],
+  rules: {
+    'next/no-css-tags': ['error'],
+
+    'next/no-html-link-for-pages': ['error'],
+
+    'next/no-sync-scripts': ['error'],
+  },
+}
