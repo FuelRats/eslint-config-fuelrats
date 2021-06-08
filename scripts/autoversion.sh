cb78126563c56740d52a2864199d8ddde96f2d57
Cameron Welter <camjwelt@gmail.com>
2021-06-08 03:48:22 +0000

    finish yarn berry migration, bump prerelease version

--- scripts/autoversion.sh
+++ scripts/autoversion.sh
@@ -6,31 +6,17 @@ CANARY=false
 for opt in "$@";
 do
   if [[ "$opt" == "--canary" || "$opt" == "-c" ]]; then
-    ARGLIST="$ARGLIST --preid canary"
     CANARY=true
   else
     ARGLIST="$ARGLIST $opt"
   fi
 done
 
+yarn workspaces foreach version $ARGLIST --deferred
+yarn version $ARGLIST --deferred
+
 if [[ "$CANARY" == true ]]; then
-  ARGLIST="${ARGLIST//--major/--premajor}"
-  ARGLIST="${ARGLIST//--minor/--preminor}"
-  ARGLIST="${ARGLIST//--patch/--prepatch}"
-  ARGLIST="${ARGLIST//--release/-prerelease}"
+  yarn version apply --all --prerelease="canary.%n"
+else
+  yarn version apply --all
 fi
-
-if ! [[ $ARGLIST =~ --((pre)?(major|minor|patch)|prerelease) ]]; then
-  if [[ "$CANARY" == true ]]; then
-    ARGLIST="$ARGLIST --prerelease"
-  else
-    ARGLIST="$ARGLIST --patch"
-  fi
-fi
-
-for dir in `find ./packages -mindepth 1 -maxdepth 1 -type d`
-do
-  yarn version $ARGLIST --cwd=$dir --no-git-tag-version --no-commit-hooks
-done
-git add -A
-yarn version $ARGLIST --no-git-tag-version
