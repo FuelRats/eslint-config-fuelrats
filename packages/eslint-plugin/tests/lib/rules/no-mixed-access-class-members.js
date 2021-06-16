925a14d47d592b09383fd27f782c3d1ca66589d3
Cameron Welter <camjwelt@gmail.com>
2021-06-16 19:43:51 +0000

    conform to ESLint config

--- packages/eslint-plugin/tests/lib/rules/no-mixed-access-class-members.js
+++ packages/eslint-plugin/tests/lib/rules/no-mixed-access-class-members.js
@@ -1,112 +1,115 @@
 /**
- * @fileoverview disallow public class members to share names with private members
+ * @file disallow public class members to share names with private members
  * @author Cameron Welter
  */
-"use strict";
+'use strict'
 
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Requirements
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/no-mixed-access-class-members");
-const RuleTester = require("../../../util/RuleTester");
+const rule = require('../../../lib/rules/no-mixed-access-class-members')
+const RuleTester = require('../../../util/RuleTester')
 
-
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
-
-const noMixedError = (name, output) => ({
+const noMixedError = (name, output) => {
+  return {
     messageId: 'noMixed',
     suggestions: [{
-        messageId: 'noMixedSuggest',
-        data: { name },
-        output,
-    }]
-})
+      messageId: 'noMixedSuggest',
+      data: { name },
+      output,
+    }],
+  }
+}
 
-var ruleTester = new RuleTester();
 
-ruleTester.run("no-mixed-access-class-members", rule, {
-    valid: [
-        "class A { #foo = 'hello'; bar () { return this.#foo } }",
-        "class A { #foo = 'hello'; bar () { this.#foo = 'bar' } }",
-        "class A { #foo = 'hello'; baz = 'biz'; bar () { this.baz = 'baz' } }",
-        "class A { static #foo = 'hello'; static baz = 'biz'; bar () { A.#foo = 'baz' } }",
-        "class A { static #foo = 'hello'; static baz = 'biz'; bar () { A.baz = 'baz' } }",
-        `class A {
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester()
+ruleTester.run('no-mixed-access-class-members', rule, {
+  valid: [
+    "class A { #foo = 'hello'; bar () { return this.#foo } }",
+    "class A { #foo = 'hello'; bar () { this.#foo = 'bar' } }",
+    "class A { #foo = 'hello'; baz = 'biz'; bar () { this.baz = 'baz' } }",
+    "class A { static #foo = 'hello'; static baz = 'biz'; bar () { A.#foo = 'baz' } }",
+    "class A { static #foo = 'hello'; static baz = 'biz'; bar () { A.baz = 'baz' } }",
+    `class A {
+      #foo = 'hello';
+      bInst = new (
+        class B {
+          #foo2 = 'hello world';
+          bar2 () {
+            return this.foo
+          }
+        }
+      )();
+      bar () {
+        return this.#foo
+      }
+    }`,
+  ],
+
+  invalid: [
+    {
+      code: "class A { #foo = 'hello'; bar () { return this.foo } }",
+      errors: [noMixedError('foo', "class A { #foo = 'hello'; bar () { return this.#foo } }")],
+    },
+
+    {
+      code: "class A { static #foo = 'hello'; bar () { return A.foo } }",
+      errors: [noMixedError('foo', "class A { static #foo = 'hello'; bar () { return A.#foo } }")],
+    },
+
+    {
+      code: `
+          class A {
             #foo = 'hello';
             bInst = new (
-                class B {
-                    #foo2 = 'hello world';
-                    bar2 () {
-                        return this.foo
-                    }
+              class B {
+                #foo2 = 'hello world';
+                bar2 () {
+                  return this.foo2
                 }
+              }
             )();
             bar () {
-                return this.#foo
+              return this.foo
             }
-        }`
-    ],
-
-    invalid: [
-        {
-            code: "class A { #foo = 'hello'; bar () { return this.foo } }",
-            errors: [noMixedError('foo', "class A { #foo = 'hello'; bar () { return this.#foo } }")]
-        },
-
-        {
-            code: "class A { static #foo = 'hello'; bar () { return A.foo } }",
-            errors: [noMixedError('foo', "class A { static #foo = 'hello'; bar () { return A.#foo } }")]
-        },
-
-        {
-            code: `class A {
-                #foo = 'hello';
-                bInst = new (
-                    class B {
-                        #foo2 = 'hello world';
-                        bar2 () {
-                            return this.foo2
-                        }
-                    }
-                )();
-                bar () {
-                    return this.foo
+          }`,
+      errors: [
+        noMixedError('foo2', `
+          class A {
+            #foo = 'hello';
+            bInst = new (
+              class B {
+                #foo2 = 'hello world';
+                bar2 () {
+                  return this.#foo2
                 }
-            }`,
-            errors: [
-            noMixedError('foo2', `class A {
-                #foo = 'hello';
-                bInst = new (
-                    class B {
-                        #foo2 = 'hello world';
-                        bar2 () {
-                            return this.#foo2
-                        }
-                    }
-                )();
-                bar () {
-                    return this.foo
+              }
+            )();
+            bar () {
+              return this.foo
+            }
+          }`),
+        noMixedError('foo', `
+          class A {
+            #foo = 'hello';
+            bInst = new (
+              class B {
+                #foo2 = 'hello world';
+                bar2 () {
+                  return this.foo2
                 }
-            }`),
-            noMixedError('foo', `class A {
-                #foo = 'hello';
-                bInst = new (
-                    class B {
-                        #foo2 = 'hello world';
-                        bar2 () {
-                            return this.foo2
-                        }
-                    }
-                )();
-                bar () {
-                    return this.#foo
-                }
-            }`)
-            ]
-        }
-
-    ]
-});
+              }
+            )();
+            bar () {
+              return this.#foo
+            }
+          }`),
+      ],
+    },
+  ],
+})
