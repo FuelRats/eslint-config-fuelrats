1af887282376bcc68ff26905041a95f94d118b6b
Cameron Welter <camjwelt@gmail.com>
2021-06-16 19:59:19 +0000

    add export-defualt-from plugin for testing new rule

--- packages/eslint-plugin/babel.config.js
+++ packages/eslint-plugin/babel.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '@babel/proposal-class-properties',
     '@babel/proposal-private-methods',
     '@babel/proposal-private-property-in-object',
-  ]
+    '@babel/proposal-export-default-from',
+  ],
 }
