a955e3ba42fb4c96dc5c390b70a31bc5884d0ce7
Cameron Welter <camjwelt@gmail.com>
2021-07-12 02:18:08 +0000

    add clarifying comments to confusing global config generator

--- packages/eslint-config/util/confusingGlobals.js
+++ packages/eslint-config/util/confusingGlobals.js
@@ -3,9 +3,11 @@ module.exports = require('confusing-browser-globals').reduce(
     switch (globProp) {
       case 'self': // Removed due to self having use in workers.
       case 'location': // Removed since location is a commonly used and accepted browser global.
+        // Globals we do not consider confusing.
         break
 
       case 'opera':
+        // Non-standard globals.
         acc.push({
           name: globProp,
           message: `${globProp} is a nonstandard browser global, and should not be used.`,
@@ -13,6 +15,7 @@ module.exports = require('confusing-browser-globals').reduce(
         break
 
       default:
+        // Confusing globals.
         acc.push({
           name: globProp,
           message: `Use of \`${globProp}\` is confusing. Use \`window.${globProp}\` instead.`,
