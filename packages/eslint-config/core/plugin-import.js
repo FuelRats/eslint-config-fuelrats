3005c6405733d762e8e0a41eab9e93dc38ca942f
Cameron Welter <camjwelt@gmail.com>
2021-07-12 01:38:44 +0000

    update no-extraneous-dependencies

--- packages/eslint-config/core/plugin-import.js
+++ packages/eslint-config/core/plugin-import.js
@@ -155,17 +155,21 @@ module.exports = {
         'test.{js,jsx}', // repos with a single test file
         'test-*.{js,jsx}', // repos with multiple top-level test files
         '**/*{.,_}{test,spec}.{js,jsx}', // tests where the extension or filename suffix denotes that it is a test
-        '**/jest.config.js', // jest config
-        '**/vue.config.js', // vue-cli config
-        '**/webpack.config.js', // webpack config
-        '**/webpack.config.*.js', // webpack config
-        '**/rollup.config.js', // rollup config
-        '**/rollup.config.*.js', // rollup config
-        '**/gulpfile.js', // gulp config
-        '**/gulpfile.*.js', // gulp config
-        '**/Gruntfile{,.js}', // grunt config
-        '**/protractor.conf.js', // protractor config
-        '**/protractor.conf.*.js', // protractor config
+        '**/.eslintrc.{js,cjs,mjs}', // eslint config file
+        '**/.babelrc.{js,cjs,mjs}', // babel config file
+        '**/jest.config.{js,cjs,mjs}', // jest config
+        '**/vue.config.{js,cjs,mjs}', // vue-cli config
+        '**/next.config.{js,cjs,mjs}', // Next config
+        '**/postcss.confg.{js,cjs,mjs}',
+        '**/webpack.config.{js,cjs,mjs}', // webpack config
+        '**/webpack.config.*.{js,cjs,mjs}', // webpack config
+        '**/rollup.config.{js,cjs,mjs}', // rollup config
+        '**/rollup.config.*.{js,cjs,mjs}', // rollup config
+        '**/gulpfile.{js,cjs,mjs}', // gulp config
+        '**/gulpfile.*.{js,cjs,mjs}', // gulp config
+        '**/Gruntfile{,.{js,cjs,mjs}}', // grunt config
+        '**/protractor.conf.{js,cjs,mjs}', // protractor config
+        '**/protractor.conf.*.{js,cjs,mjs}', // protractor config
       ],
       optionalDependencies: true,
       peerDependencies: false,
